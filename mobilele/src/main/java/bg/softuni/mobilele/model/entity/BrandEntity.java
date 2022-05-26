f558bac2db44901759547cd86c8db58dfd8c89cf
luchob <lachezar.balev@gmail.com>
2022-05-26 21:06:15 +0300

    First objects

--- mobilele/src/main/java/bg/softuni/mobilele/model/entity/BrandEntity.java
+++ mobilele/src/main/java/bg/softuni/mobilele/model/entity/BrandEntity.java
@@ -1,14 +1,46 @@
 package bg.softuni.mobilele.model.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "brands")
 public class BrandEntity extends BaseEntity{
 
+  @Column(nullable = false)
   private String name;
 
+  @OneToMany(
+      mappedBy = "brand",
+      fetch = FetchType.EAGER,
+      cascade = CascadeType.ALL
+  )
+  private List<ModelEntity> models = new ArrayList<>();
 
+  public String getName() {
+    return name;
+  }
 
+  public BrandEntity setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public List<ModelEntity> getModels() {
+    return models;
+  }
+
+  public BrandEntity setModels(List<ModelEntity> models) {
+    this.models = models;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "BrandEntity{" +
+        "name='" + name + '\'' +
+        ", models=" + models +
+        '}';
+  }
 }
--- mobilele/src/main/java/bg/softuni/mobilele/model/entity/ModelEntity.java
+++ mobilele/src/main/java/bg/softuni/mobilele/model/entity/ModelEntity.java
@@ -1,26 +1,93 @@
 package bg.softuni.mobilele.model.entity;
 
 import bg.softuni.mobilele.model.entity.enums.CategoryEnum;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Table;
 
-//TODO - example with UUID!!!!
+import javax.persistence.*;
 
 @Entity
 @Table(name = "models")
 public class ModelEntity extends BaseEntity {
 
+  @Column(nullable = false)
   private String name;
 
+  @Column(nullable = false)
   @Enumerated(EnumType.STRING)
   private CategoryEnum category;
 
+  @Column(nullable = false)
   private String imageUrl;
 
   private int startYear;
 
   private Long endYear;
 
+  @ManyToOne
+  private BrandEntity brand;
+
+  public String getName() {
+    return name;
+  }
+
+  public ModelEntity setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public CategoryEnum getCategory() {
+    return category;
+  }
+
+  public ModelEntity setCategory(CategoryEnum category) {
+    this.category = category;
+    return this;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public ModelEntity setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+    return this;
+  }
+
+  public int getStartYear() {
+    return startYear;
+  }
+
+  public ModelEntity setStartYear(int startYear) {
+    this.startYear = startYear;
+    return this;
+  }
+
+  public Long getEndYear() {
+    return endYear;
+  }
+
+  public ModelEntity setEndYear(Long endYear) {
+    this.endYear = endYear;
+    return this;
+  }
+
+  public BrandEntity getBrand() {
+    return brand;
+  }
+
+  public ModelEntity setBrand(BrandEntity brand) {
+    this.brand = brand;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "ModelEntity{" +
+        "name='" + name + '\'' +
+        ", category=" + category +
+        ", imageUrl='" + imageUrl + '\'' +
+        ", startYear=" + startYear +
+        ", endYear=" + endYear +
+        ", brand=" + (brand != null ? brand.getName() : null) +
+        '}';
+  }
 }
