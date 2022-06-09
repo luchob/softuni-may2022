82e95e878d5bcda12cefd9e0c312c1589b5c5581
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-06-09 12:38:42 +0300

    Fixed login page default values that you always have to delete from the input fields in order to input your credentials. Fixed username input field to now display email label and placeholders in order to not confuse users, cuz this is actually how we log in. Added login fields validations. The previous state was - input invalid credentials? Error page. Credentials are now validated not only in the DTO, but in the login service as well and we now actually use the boolean value of the login service method - in case of invalid pass error message will be shown as well. Without it we get error page instead.

--- mobilele/src/main/java/bg/softuni/mobilele/web/UserController.java
+++ mobilele/src/main/java/bg/softuni/mobilele/web/UserController.java
@@ -6,8 +6,10 @@ import bg.softuni.mobilele.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -19,6 +21,11 @@ public class UserController {
 
   public UserController(UserService userService) {
     this.userService = userService;
+  }
+
+  @ModelAttribute("userModel")
+  public UserLoginDTO initUserModel() {
+    return new UserLoginDTO();
   }
 
   @GetMapping("/login")
@@ -33,8 +40,19 @@ public class UserController {
   }
 
   @PostMapping("/login")
-  public String login(UserLoginDTO userLoginDTO) {
-    userService.login(userLoginDTO);
+  public String login(@Valid UserLoginDTO userLoginDTO,
+                      BindingResult bindingResult,
+                      RedirectAttributes redirectAttributes
+                      ) {
+
+    if (bindingResult.hasErrors() || !this.userService.login(userLoginDTO)) {
+      redirectAttributes.addFlashAttribute("userModel", userLoginDTO);
+      redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userModel",
+              bindingResult);
+      bindingResult.rejectValue("password", "InvalidPasswordError", "Invalid password.");
+      return "redirect:/users/login";
+    }
+
     return "redirect:/";
   }
 }
