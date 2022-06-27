e83927e8aadb7171d875094bbb56ade32b4527da
luchob <lachezar.balev@gmail.com>
2022-06-27 21:33:19 +0300

    Sample API consumption.

---
+++ resttemplete/src/main/java/bg/softuni/resttemplete/ResttempleteApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.resttemplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttempleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttempleteApplication.class, args);
+	}
+
+}
---
+++ resttemplete/src/test/java/bg/softuni/resttemplete/ResttempleteApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.resttemplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttempleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
