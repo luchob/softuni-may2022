1e27943581943860aed1cea83c96491cdfa46bb9
OKuzmanov <okuzmanov@outlook.com>
2022-06-28 21:19:17 +0300

    BookController - Edit API Endpoint Added
    Добавих Edit API endpoint, без валидация и малко спорен save метод в AuthorService - защото връща директно AuthorEntity вместо ДТО, но съм го използвал и в Creat.

--- books/src/main/java/bg/softuni/books/service/BookService.java
+++ books/src/main/java/bg/softuni/books/service/BookService.java
@@ -62,6 +62,43 @@ public class BookService {
     return entityToBeSaved.getId();
   }
 
+  public BookDTO persistBook(Long id, BookDTO bookDTO) {
+
+    Optional<BookEntity> bookOpt = this.bookRepository.findById(id);
+
+    if(bookOpt.isEmpty()) {
+      return null;
+    }
+
+    BookEntity bookEntity = bookOpt.get();
+
+    updateBookEntity(bookEntity, bookDTO);
+
+    this.bookRepository.save(bookEntity);
+
+    return this.map(bookEntity);
+  }
+
+  private void updateBookEntity(BookEntity bookEntity, BookDTO bookDTO) {
+    String title = bookDTO.getTitle();
+    if(title != null) {
+      bookEntity.setTitle(title);
+    }
+
+    String isbn = bookDTO.getIsbn();
+    if(isbn != null) {
+      bookEntity.setIsbn(isbn);
+    }
+
+    if(bookDTO.getAuthor() != null) {
+      Optional<AuthorEntity> authorOpt = this.authorService.findAuthorByName(bookDTO.getAuthor().getName());
+
+      bookEntity.setAuthor(authorOpt.isEmpty()
+              ? this.authorService.save(new AuthorEntity().setName(bookDTO.getAuthor().getName()))
+              : authorOpt.get());
+    }
+  }
+
   private BookDTO map(BookEntity bookEntity) {
     return new
         BookDTO().
@@ -70,5 +107,4 @@ public class BookService {
           setIsbn(bookEntity.getIsbn()).
         setAuthor(new AuthorDTO().setName(bookEntity.getAuthor().getName()));
   }
-
 }
