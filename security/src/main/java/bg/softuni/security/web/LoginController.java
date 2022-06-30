cd0eb11b1c25f2660fc8664e2f3487e26418e8dc
luchob <lachezar.balev@gmail.com>
2022-06-30 15:44:45 +0300

    Prepare exercise.

---
+++ security/src/main/java/bg/softuni/security/web/LoginController.java
@@ -0,0 +1,18 @@
+package bg.softuni.security.web;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LoginController {
+
+  @GetMapping("/users/login")
+  public String login() {
+    return "auth-login";
+  }
+
+}
